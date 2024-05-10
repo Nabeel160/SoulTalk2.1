464167bd603b804f36eac947e00d3d07f6fc5102
tanfarrukh007 <FarrukhT26@gmail.com>
2024-05-10 11:36:58 +0500

    commitment

--- soulTalk_frontend/src/components/pages/Psychologist/PsychologistList.tsx
+++ soulTalk_frontend/src/components/pages/Psychologist/PsychologistList.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setPsychologist, selectPsychologist } from '../../../reduxStore/slice/psychologistSlice';
-import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import "../../../styles/psychologist.css";
@@ -26,32 +25,38 @@ const PsychologistList = () => {
   };
 
   return (
-    <Container className='stylings'>
-      <Row>
-        {loading ? (
-          <div className="loader"></div>
-        ) : (
-          psychologists?.map((psychologist: { id: React.Key | null | undefined; image: string | undefined; first_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; last_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; user: { email: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }; age: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; qualification: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; reviews: any[]; }) => (
-            <Col xs={12} md={6} lg={4} key={psychologist.id}>
-              <Card className="psychologist-card">
-                <Card.Img variant="top" src={psychologist.image} />
-                <Card.Body className="card-body-custom">
-                  <Card.Title>{psychologist.first_name} {psychologist.last_name}</Card.Title>
-                  <Card.Text><b>Email:</b> {psychologist.user.email}</Card.Text>
-                  <Card.Text><b>Age:</b> {psychologist.age}</Card.Text>
-                  <Card.Text><b>Qualification:</b> {psychologist.qualification}</Card.Text>
-                  {Array.isArray(psychologist.reviews) && psychologist.reviews.length > 0 && (
-                    <h6>Average Rating: {(psychologist.reviews.reduce((total: any, review: { rating: any; }) => total + review.rating, 0) / psychologist.reviews.length).toFixed(2)}</h6>
-                  )}
-                  <hr />
-                  <Link  className='btn btn-success btn-view' to={`/Psychologist/PsychologistDetail/${psychologist.id}`} onClick={() => handlePsychologistClick(psychologist)}>View</Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))
-        )}
-      </Row>
-    </Container>
+   <div className='stylings container'>
+  <div className="row d-flex  flex-wrap gap-2 gap-lg-0">
+    {loading ? (
+      <div className="loader"></div>
+    ) : (
+      psychologists?.map((psychologist: { id: React.Key | null | undefined; image: string | undefined; first_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; last_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; user: { email: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }; age: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; qualification: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; reviews: any[]; }) => (
+        <div className="col-12 col-sm-6 col-md-5 col-lg-4" key={psychologist.id}>
+          <div className="flip-card  ">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img className="title" src={psychologist.image} alt="Psychologist" />
+              </div>
+              <div className="flip-card-back ">
+                <p className="title text-uppercase" ><u>{psychologist.first_name} {psychologist.last_name}</u></p>
+                <h5 className="text-uppercase"><u>Details</u></h5>
+                <p style={{fontSize:"15px"}}><b>Email:</b> {psychologist.user.email}</p>
+                <p style={{fontSize:"15px"}}><b>Age:</b> {psychologist.age}</p>
+                <p><b>Qualification:</b> {psychologist.qualification}</p>
+                {Array.isArray(psychologist.reviews) && psychologist.reviews.length > 0 && (
+                  <h6>Average Rating: {(psychologist.reviews.reduce((total: any, review: { rating: any; }) => total + review.rating, 0) / psychologist.reviews.length).toFixed(2)}</h6>
+                )}
+
+                <Link className='btn btn-success btn-view ' style={{fontSize:"20px",backgroundColor:"#19a7ce",textAlign:"center"}} to={`/Psychologist/PsychologistDetail/${psychologist.id}`} onClick={() => handlePsychologistClick(psychologist)}>View</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))
+    )}
+  </div>
+</div>
+
   );
 };
 
